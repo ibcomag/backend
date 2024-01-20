cad5179c828e9aacd49785c20e9ac349b9457b78
Pedro <pedrojuniorifpb@gmail.com>
2024-01-20 20:40:02 -0300

    Mudança temporaria para adicionar data por data de criação

--- src/notion/services/articles.ts
+++ src/notion/services/articles.ts
@@ -1,4 +1,3 @@
-
 import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { getIdInPage, getValues } from "../../services/objResponseNotion";
 import notion from "../main";
@@ -12,22 +11,24 @@ export const getAllArticles = async () => {
     });
     const results = response.results.map((obj: PageObjectResponse) => {
         const newObj: ArticlesType = getValues(obj.properties);
-        return { ...newObj, id: obj.id };
+        return { ...newObj, id: obj.id, date: obj.created_time };
     });
     return results;
 };
 
 export const getArticlesById = async ({ id }: Partial<ArticlesType>) => {
-    const response: Partial<PageObjectResponse> = await notion.pages.retrieve({
-        page_id: id,
-    });
-    const dataResponse: ArticlesType = getValues(response.properties);
-    const idPageContent = getIdInPage(dataResponse);
+    // const response: Partial<PageObjectResponse> = await notion.pages.retrieve({
+    //     page_id: id,
+    // });
+    const allArticles = await getAllArticles()
+    const response = allArticles.find(({ id }) => id === id)
+    // const dataResponse: ArticlesType = getValues(response.properties);
+    const idPageContent = getIdInPage(response);
     const responsePage = await notion.blocks.children.list({
         block_id: idPageContent,
     });
     return {
-        ...dataResponse,
+        ...response,
         content: responsePage.results,
     };
 };
